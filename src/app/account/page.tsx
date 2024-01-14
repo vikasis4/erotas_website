663f6dd6c8cc605aa32ccede46ea19a145fe2b0f
vikas <vikasisgen@gmail.com>
2024-01-15 00:26:42 +0530

    logout

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,24 +1,32 @@
 'use client'
 import React from 'react'
-import { handleLogOut } from '@/utils/logout'
+import { setLogOut } from '@/redux/slice/general'
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
 import { useRouter } from 'next/navigation'
 
 
 function page() {
 
-    // const router = useRouter();
-    // const state = useAppSelector((state:any) => state.general.isAuthenticated)
-    // const dispatch = useAppDispatch();
-    // if (!state) {
-    //     router.push('/')
-    // }
+    const router = useRouter();
+    const state = useAppSelector((state: any) => state.general.isAuthenticated)
+    const dispatch = useAppDispatch();
+    if (!state) {
+        router.push('/')
+    }
+    const handleLogOut = () => {
+        if (typeof localStorage !== 'undefined') {
+            localStorage.removeItem('JWT_token')
+        };
+        dispatch(setLogOut());
+        router.refresh();
+        router.push('/');
+    }
 
     return (
         <div className="flex h-full  justify-center items-center">
-            {/* <button
-                onClick={() => handleLogOut({ dispatch, router })}
-                className='font-poppin text-bold text-white bg-red-600 py-6 px-12 rounded text-3xl shadow-xl'>Logout</button> */}
+            <button
+                onClick={() => handleLogOut()}
+                className='font-poppin text-bold text-white bg-red-600 py-6 px-12 rounded text-3xl shadow-xl'>Logout</button>
         </div>
     )
 }

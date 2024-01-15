f51ef8e72d8ca3e4bc7d330d98de04959ad86b99
vikas <vikasisgen@gmail.com>
2024-01-15 09:10:10 +0530

    product listing update

--- src/components/ProductMap.tsx
+++ src/components/ProductMap.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation' 
 import React from 'react'
 import { ProductBaseURL } from '@/config/apis'
 
@@ -13,11 +13,11 @@ function ProductMap({ data }: any) {
 
     return (
         <div onClick={() => router.push(`/product/${productId}`)} 
-        className=" hover:cursor-pointer flex flex-col justify-center items-center shadow-md md:w-[40%] py-4 overflow-hidden rounded-xl bg-white md:hover:scale-125 duration-300 ">
-            <Image loader={myLoader} src={ProductBaseURL + imagesLink[0]} alt="Product" height={200} width={200} />
+        className=" hover:cursor-pointer flex flex-col justify-center items-center w-[45%] shadow-md md:w-[40%] py-4 overflow-hidden rounded-xl bg-white md:hover:scale-125 duration-300 ">
+            <Image loader={myLoader} src={ProductBaseURL + imagesLink[0]} alt="Product" height={100} width={100} />
             <div className="px-4 bg-white">
-                <h1 className="text-lg font-poppin text-black text-center py-1">{name}</h1>
-                <h1 className="text-2xl font-poppin font-normal  text-green-700 text-center py-1">&#8377; {price} /-</h1>
+                <h1 className="text-xs md:text-lg font-poppin text-black text-center py-1">{name}</h1>
+                <h1 className="text-xl md:text-2xl font-poppin font-normal  text-green-700 text-center py-1">&#8377; {price} /-</h1>
             </div>
         </div>
     )

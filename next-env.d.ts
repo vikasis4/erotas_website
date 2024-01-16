ea89fa5ff69b314737b9db3102eb4abbcad6f654
vikas <vikasisgen@gmail.com>
2024-01-16 22:42:26 +0530

    pay type change

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,2 @@
-import Razorpay from 'razorpay';
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-
-declare global {
-    interface Window {
-      Razorpay: any;
-    }
-  }
